84745ce2331b8790d8ec6a4a4beb381ea690b20b
hyeonguj <jhg3182@naver.com>
2019-12-19 01:21:11 +0900

    init project

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # spring-boot-vuejs-gradle
+아래 프로젝트의 기능들을 gradle로 사용할수 있도록 제작하였습니다.
+
+https://github.com/jonashackt/spring-boot-vuejs.git
+
+## install
+gradle
+java
+node
+
+## run
+backend : run spring boot application
+frontend : npm run serve
+
+## port
+
+backend : 8089
+frontend : 8088
---
+++ backend/src/main/java/com/hyeonguj/springbootvuejs/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.hyeonguj.springbootvuejs.repository;
+
+
+import com.hyeonguj.springbootvuejs.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    List<User> findByLastName(@Param("lastname") String lastname);
+
+    List<User> findByFirstName(@Param("firstname") String firstname);
+
+}
